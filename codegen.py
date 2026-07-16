5623361b0b1a7fd862e9e93cb2ca2a91349e1437
Sebastian Fišer <sebastianfiser2275@gmail.com>
2026-07-16 20:02:34 +0200

    attempt at data differencing

--- codegen.py
+++ codegen.py
@@ -4,8 +4,16 @@ def generate(ast):
     lines.append("int main() {")
 
     for node in ast["body"]:
+        if node ["type"] == "let":
+            if node["d_type"] == "whole":
+                data_type = "int"
+            elif node["d_type"] == "words":
+                data_type = "char*"
+            else:
+                data_type = node["d_type"]
+
         if node["type"] == "let":
-            lines.append(f"     int {node['name']} = {node['value']};")
+            lines.append(f"     {data_type} {node['name']} = {node['value']};")
         elif node["type"] == "print":
             lines.append(f"     printf(\"{node['value']}\");")
         elif node["type"] == "return":
